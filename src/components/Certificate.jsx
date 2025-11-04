b2f42000de05c3a13c5a821a595d9ec75772ad10
zohaib khalid <zohaibchaudry6@gmail.com>
2025-11-04 21:10:44 +0500

    add some data

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -73,19 +73,19 @@ export const CertificateDetails = ({ id }) => {
     className='font-bahij -mt-10 font-medium'>
     
   <Section title='بيانات الشهادة:' >
-    <KeyValue label="رقم الشهادة" value={certificate.certificateNumber.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="تاريخ الإصدار" value={certificate.issueDate.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="نهاية الصلاحية" value={certificate.expiryDate.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="رقم الشهادة" value={certificate.certificateNumber.substring(0,20)   } />
+    <KeyValue label="تاريخ الإصدار" value={certificate.issueDate.substring(0,20)   } />
+    <KeyValue label="نهاية الصلاحية" value={certificate.expiryDate.substring(0,20)   } />
   </Section>
 
   <Section title="بيانات صاحب الشهادة: ">
-    <KeyValue label="الاسم" value={certificate.customerName.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="رقم الهوية" value={certificate.idNumber.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="الجنس" value={certificate.gender.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="الجنسية" value={certificate.nationality.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="المهنة" value={certificate.profession.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="البرنامج التثقيفي" value={certificate.educationalProgram.substring(0,20)  .substring(0,20)  } />
-    <KeyValue label="تاريخ إنهاء البرنامج" value={certificate.programCompletionDate.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="الاسم" value={certificate.customerName.substring(0,20)   } />
+    <KeyValue label="رقم الهوية" value={certificate.idNumber.substring(0,20)   } />
+    <KeyValue label="الجنس" value={certificate.gender.substring(0,20)   } />
+    <KeyValue label="الجنسية" value={certificate.nationality.substring(0,20)   } />
+    <KeyValue label="المهنة" value={certificate.profession.substring(0,20)   } />
+    <KeyValue label="البرنامج التثقيفي" value={certificate.educationalProgram.substring(0,20)   } />
+    <KeyValue label="تاريخ إنهاء البرنامج" value={certificate.programCompletionDate.substring(0,20)   } />
   </Section>
 
 
@@ -106,7 +106,7 @@ export const CertificateDetails = ({ id }) => {
 const KeyValue = ({ label, value }) => (
   <div className="row-1">
 
-  <div className="flex justify-evenly ml-40 space-x-20 py-2  last:border-none border-2 border-amber-500">
+  <div className="flex justify-evenly ml-32 space-x-10 py-2  last:border-none border-2 border-amber-500">
     <span className="text-gray-500">{label}</span>
     <span className=" text-black">{value}</span>
   </div>
