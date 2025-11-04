09f935fdab60877e0b87e5c964e41e4df842c42c
zohaib khalid <zohaibchaudry6@gmail.com>
2025-11-04 21:04:47 +0500

    add some changes

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -68,30 +68,30 @@ export const CertificateDetails = ({ id }) => {
     
 
       {/* Data Sections */}
-<div dir="rtl" className="space-y-0 text-right">
+<div dir="rtl" className="space-y-10 text-right">
   <div 
     className='font-bahij -mt-10 font-medium'>
     
   <Section title='بيانات الشهادة:' >
-    <KeyValue label="رقم الشهادة" value={certificate.certificateNumber.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="تاريخ الإصدار" value={certificate.issueDate.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="نهاية الصلاحية" value={certificate.expiryDate.substring(0,15)  .substring(0,15)  } />
+    <KeyValue label="رقم الشهادة" value={certificate.certificateNumber.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="تاريخ الإصدار" value={certificate.issueDate.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="نهاية الصلاحية" value={certificate.expiryDate.substring(0,20)  .substring(0,20)  } />
   </Section>
 
   <Section title="بيانات صاحب الشهادة: ">
-    <KeyValue label="الاسم" value={certificate.customerName.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="رقم الهوية" value={certificate.idNumber.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="الجنس" value={certificate.gender.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="الجنسية" value={certificate.nationality.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="المهنة" value={certificate.profession.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="البرنامج التثقيفي" value={certificate.educationalProgram.substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="تاريخ إنهاء البرنامج" value={certificate.programCompletionDate.substring(0,15)  .substring(0,15)  } />
+    <KeyValue label="الاسم" value={certificate.customerName.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="رقم الهوية" value={certificate.idNumber.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="الجنس" value={certificate.gender.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="الجنسية" value={certificate.nationality.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="المهنة" value={certificate.profession.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="البرنامج التثقيفي" value={certificate.educationalProgram.substring(0,20)  .substring(0,20)  } />
+    <KeyValue label="تاريخ إنهاء البرنامج" value={certificate.programCompletionDate.substring(0,20)  .substring(0,20)  } />
   </Section>
 
 
   <Section title="بيانات المنشأة:">
-    <KeyValue label="اسم المنشأة" value={certificate.establishmentName || "----".substring(0,15)  .substring(0,15)  } />
-    <KeyValue label="رقم رخصة المنشأة" value={certificate.establishmentLicense || "----".substring(0,15)  .substring(0,15)  } />
+    <KeyValue label="اسم المنشأة" value={certificate.establishmentName.substring(0,20) || "----" } />
+    <KeyValue label="رقم رخصة المنشأة" value={certificate.establishmentLicense.substring(0,20) || "----"    } />
   </Section>
   
   </div>
@@ -102,18 +102,22 @@ export const CertificateDetails = ({ id }) => {
   );
 };
 
-// مكوّن مساعد لعناصر “تسمية + قيمة”
+// // مكوّن مساعد لعناصر “تسمية + قيمة”
 const KeyValue = ({ label, value }) => (
-  <div className="flex justify-between py-2  last:border-none">
+  <div className="row-1">
+
+  <div className="flex justify-evenly ml-40 space-x-20 py-2  last:border-none border-2 border-amber-500">
     <span className="text-gray-500">{label}</span>
     <span className=" text-black">{value}</span>
   </div>
+  </div>
 );
+
 
 // مكوّن مساعد للقسم
 const Section = ({ title, children }) => (
-  <div className="p-3 ">
-    <h3 className="text-green-800 font-semibold mb-4 text-xl border-t border-gray-300 py-2">{title}</h3>
+  <div className="py-3">
+    <h3 className="text-green-800 font-semibold mb-4 text-xl  border-t border-gray-300 py-2">{title}</h3>
     {children}
   </div>
 );
