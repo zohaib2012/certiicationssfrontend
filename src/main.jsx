1341fe5b709b52f2dfc6f7326b54303cda6923cb
zohaib khalid <zohaibchaudry60@gmail.com>
2026-03-27 22:26:44 +0500

    uncomment app
    uncomment app

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Provider store={store}>
-    {/* <App /> */}
+    <App />
     </Provider>
     </BrowserRouter>
   </StrictMode>
